b8a0026786f307ba78ad7826b15dea50e4d7befc
Reza Basereh <basereh@mahsan.net>
2023-09-12 08:52:31 +0330

    feat: setup JDBC in project

--- src/main/java/org/basereh/Main.java
+++ src/main/java/org/basereh/Main.java
@@ -3,17 +3,27 @@ package org.basereh;
 import org.basereh.dao.AuthorDao;
 import org.basereh.service.AuthorService;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         try (Scanner scanner = new Scanner(System.in)) {
-            AuthorDao authorDao = new AuthorDao();
+            try (Connection con = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/library", "root", "root")) {
+                try (Statement stmt = con.createStatement()) {
+                    AuthorDao authorDao = new AuthorDao(stmt);
 
-            AuthorService authorService = new AuthorService(authorDao);
+                    AuthorService authorService = new AuthorService(authorDao);
 
-            CLI cli = new CLI(scanner, authorService);
-            cli.mainLoop();
+                    CLI cli = new CLI(scanner, authorService);
+                    cli.mainLoop();
+                }
+
+            }
         }
     }
 }
--- src/main/java/org/basereh/dao/AuthorDao.java
+++ src/main/java/org/basereh/dao/AuthorDao.java
@@ -1,11 +1,16 @@
 package org.basereh.dao;
 
+import lombok.RequiredArgsConstructor;
 import org.basereh.domain.Author;
 
+import java.sql.Statement;
 import java.util.Arrays;
 import java.util.List;
 
+@RequiredArgsConstructor
 public class AuthorDao implements ObjectDao<Author> {
+    private final Statement statement;
+
     @Override
     public List<Author> getAll() {
         return Arrays.asList(
